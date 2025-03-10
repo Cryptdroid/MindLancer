ad3e5a2852c9be56d1c51317173fcfbf5b3feb0a
Sanish <sanishpagui162@gmail.com>
2025-03-10 17:52:15 +0530

    hosting db

--- server/middlewares/AuthMiddleware.js
+++ server/middlewares/AuthMiddleware.js
@@ -2,17 +2,13 @@ import jwt from "jsonwebtoken"; // or use: const jwt = require("jsonwebtoken");
 
 export const verifyToken = (req, res, next) => {
   try {
-    console.log("Headers Cookie:", req.headers.cookie);
     const cookies = req.headers.cookie?.split("; ").reduce((acc, cookie) => {
       const [key, value] = cookie.split("=");
       acc[key] = decodeURIComponent(value);
       return acc;
     }, {});
 
-    console.log("Parsed Cookies:", cookies);
-
     const token = JSON.parse(cookies?.jwt || "{}").jwt;
-    console.log("Extracted Token:", token);
 
     if (!token) return res.status(401).send("You are not authenticated!");
 
@@ -22,7 +18,6 @@ export const verifyToken = (req, res, next) => {
       next();
     });
   } catch (error) {
-    console.error("Error in verifyToken:", error.message);
     return res.status(500).send("Internal Server Error");
   }
 };
